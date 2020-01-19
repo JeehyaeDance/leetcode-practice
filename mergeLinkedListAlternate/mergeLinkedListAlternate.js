d9eb66b09ca9014f495c6119e87c8c160441d21f
Jeehyae dance <jeehyae91@gmail.com>
2020-01-18 23:52:35 -0800

    atempt 1

--- mergeLinkedListAlternate/mergeLinkedListAlternate.js
+++ mergeLinkedListAlternate/mergeLinkedListAlternate.js
@@ -12,6 +12,69 @@ Output :
 Remaining List : 18 -> 21 -> null
 */
 
-function mergeLinkedListAlternate() {
-  // Your Code Here
+class Node {
+  constructor(d) {
+      this.val = d;
+      this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+      this.head = null;
+  }
+
+  add(d) {
+      var end = new Node(d);
+      var current;
+      if (this.head === null) {
+          this.head = end;
+      } else {
+          current = this.head;
+          while (current.next) {
+              current = current.next;
+          }
+          current.next = end;
+      }
+  }
+}
+
+var list1 = new LinkedList();
+var list2 = new LinkedList();
+
+list1.add(5);
+list1.add(10);
+list1.add(15);
+list1.add(20);
+list1.add(25);
+list2.add(3);
+list2.add(6);
+list2.add(9);
+list2.add(12);
+list2.add(15);
+list2.add(18);
+list2.add(21);
+
+function mergeLinkedListAlternate(LL1, LL2) {
+  let result = {};
+  let leftover;
+  if (!LL1 && !LL2) {
+    return null;
+  } else {
+    result.head = 
+    while (LL1 && LL2) {
+      result.val = LL1.val;
+      result.next = LL2;
+      LL1 = LL1.next;
+      LL2 = LL2.next;
+    }
+    if (LL1) {
+      leftover = LL1;
+    } else {
+      leftover = LL2;
+    }
+  }
+  return result, leftover;      
 };
+
+console.log(list1, list2);
